a3c0b2fdb718b88d77b0ee08727d265a50958bab
fanixk <katsimpas.f@gmail.com>
2015-07-10 23:35:35 +0300

    add module list-scan

--- scan.py
+++ scan.py
@@ -8,6 +8,8 @@ import volatility.plugins.connscan as connscan
 import volatility.plugins.sockets as sockets
 import volatility.plugins.sockscan as sockscan
 import volatility.plugins.connections as connections
+import volatility.plugins.modules as modules
+import volatility.plugins.modscan as modscan
 import volatility.utils as utils
 import volatility.protos as protos
 from geoip import geolite2
@@ -30,12 +32,6 @@ def init_volatility_config():
 
 
 def get_diffs(list, scan):
-    # if scan_module == 'PSList':
-    #     for i in scanner.calculate():
-    #         print i.ImageFileName
-    #         print i.InheritedFromUniqueProcessId
-    #         print i.CreateTime
-
     list_addrs = set(list.keys())
     scan_addrs = set(scan.keys())
 
@@ -61,12 +57,20 @@ def check_instances(list):
     possible_infections = []
 
     for i in list:
+        # get name of running process
         process_name = str(list[i].ImageFileName)
+
         # pid = list[i].UniqueProcessId
+        # ppid = list[i].InheritedFromUniqueProcessId
+        # start_time = list[i].CreateTime
         # print process_name, pid, processes.count(process_name)
+
+        # calc number of process instances and save it in a dictionary
         instances[process_name] = processes.count(process_name)
 
     for ps in instances:
+        # check if correct number of instances are running for each process
+        # save incorrent counts in a list to be returned
         if rules.has_key(ps) and rules[ps]['instances'] != instances[ps]:
             possible_infections.append(ps)
 
@@ -76,17 +80,18 @@ def check_instances(list):
 def main():
     init_volatility_config()
 
+    # Get pslist & psscan differences
     pslist = lister(taskmods, 'PSList')
     psscan = scanner(filescan, 'PSScan')
-
     process_diffs = get_diffs(pslist, psscan)
+
     for diff in process_diffs:
         print 'Possible malware at process %s with PID %s. (pslist-psscan difference)' \
             %(diff.ImageFileName, diff.UniqueProcessId)
 
+    # Get connections-connscan differences
     conns = lister(connections, 'Connections')
     connscanner = scanner(connscan, 'ConnScan')
-
     conn_diffs = get_diffs(conns, connscanner)
 
     for diff in conn_diffs:
@@ -95,23 +100,29 @@ def main():
         country = geolite2.lookup(ip).country
         print 'Possible malicious connection at %s: %s (Country: %s)' %(ip, port, country)
 
+    # Get sockets-sockscan differences
     socks = lister(sockets, 'Sockets')
     sockscanner = scanner(sockscan, 'SockScan')
-
     sock_diffs = get_diffs(socks, sockscanner)
 
-    # print protos.protos
     for diff in sock_diffs:
         protocol = protos.protos.get(diff.Protocol.v(), '-')
         print 'Possible malicious socket connection from Address-%s, Port-%s, Protocol-%s, PID-%s at %s' \
                 %(diff.LocalIpAddress, diff.LocalPort, protocol, diff.Pid, diff.CreateTime)
 
+    # Get modules-modscanner differences
+    mods = lister(modules, 'Modules')
+    modscanner = scanner(modscan, 'ModScan')
+    mod_diffs = get_diffs(mods, modscanner)
+
+    for diff in mod_diffs:
+        print 'Possible malicious module %s' %(diff.Name)
+
+    # Check number of process running instances
     possible_infections = check_instances(psscan)
     for inf in possible_infections:
         print inf
 
-    # for i in rules:
-    #     print rules[i]['start_time']
 
 if __name__== '__main__':
     main()
